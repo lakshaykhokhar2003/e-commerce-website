e3bb742985cd213a7b831cdd8c16f2579b377758
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-07-29 17:11:37 +0530

    fixed: CORS error

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "https://cobradev2.kinde.com",
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
