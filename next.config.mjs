e97d366b61fad1f8d0091eb74382c1afc381733a
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-07-30 00:52:07 +0530

    fixed: minor bugs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [{
             // matching all API routes
             source: "/api/:path*", headers: [{key: "Access-Control-Allow-Credentials", value: "true"}, {
-                key: "Access-Control-Allow-Origin", value: "*"
+                key: "Access-Control-Allow-Origin", value: "https://e-commerce-website-seven-eta.vercel.app"
             }, // replace this your actual origin
                 {key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT"}, {
                     key: "Access-Control-Allow-Headers",
