d571781ba60ba59d9ece78976c70a622603b0035
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-09-17 16:47:41 +0530

    fixed: minor bugs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,19 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
-import { buttonVariants } from './ui/button'
-import { ArrowRight } from 'lucide-react'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {buttonVariants} from './ui/button'
+import {ArrowRight} from 'lucide-react'
+import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
+import {RegisterLink, LoginLink, LogoutLink,} from "@kinde-oss/kinde-auth-nextjs/components";
 
 const Navbar = async () => {
-    const { getUser } = getKindeServerSession()
+    const {getUser} = getKindeServerSession()
     const user = await getUser()
 
     const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
     return (
-        <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+        <nav
+            className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
             <MaxWidthWrapper>
                 <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
                     <Link href='/' className='flex z-40 font-semibold'>
@@ -21,14 +23,14 @@ const Navbar = async () => {
                     <div className='h-full flex items-center space-x-4'>
                         {user ? (
                             <>
-                                <Link
+                                <LogoutLink
                                     href='/api/auth/logout'
                                     className={buttonVariants({
                                         size: 'sm',
                                         variant: 'ghost',
                                     })}>
                                     Sign out
-                                </Link>
+                                </LogoutLink >
                                 {isAdmin ? (
                                     <Link
                                         href='/dashboard'
@@ -46,30 +48,30 @@ const Navbar = async () => {
                                         className: 'hidden sm:flex items-center gap-1',
                                     })}>
                                     Create case
-                                    <ArrowRight className='ml-1.5 h-5 w-5' />
+                                    <ArrowRight className='ml-1.5 h-5 w-5'/>
                                 </Link>
                             </>
                         ) : (
                             <>
-                                <Link
+                                <RegisterLink
                                     href='/api/auth/register'
                                     className={buttonVariants({
                                         size: 'sm',
                                         variant: 'ghost',
                                     })}>
                                     Sign up
-                                </Link>
+                                </RegisterLink>
 
-                                <Link
+                                <LoginLink
                                     href='/api/auth/login'
                                     className={buttonVariants({
                                         size: 'sm',
                                         variant: 'ghost',
                                     })}>
                                     Login
-                                </Link>
+                                </LoginLink>
 
-                                <div className='h-8 w-px bg-zinc-200 hidden sm:block' />
+                                <div className='h-8 w-px bg-zinc-200 hidden sm:block'/>
 
                                 <Link
                                     href='/configure/upload'
@@ -78,7 +80,7 @@ const Navbar = async () => {
                                         className: 'hidden sm:flex items-center gap-1',
                                     })}>
                                     Create case
-                                    <ArrowRight className='ml-1.5 h-5 w-5' />
+                                    <ArrowRight className='ml-1.5 h-5 w-5'/>
                                 </Link>
                             </>
                         )}
